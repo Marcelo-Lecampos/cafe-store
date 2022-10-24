3b42830a2d79409539085d00f040e37da187eb43
Marcelo Leão <marcelo.lecampos@gmail.com>
2022-10-24 17:14:37 -0300

    🎉 Commit inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(<div><Component {...pageProps} />
+  <footer>Testei.com</footer>
+  </div> ) 
 }
 
 export default MyApp
